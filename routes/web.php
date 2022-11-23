f860959d5862e7e20200e2465b3bd64f85dfe18f
Astra0023 <tambajhoncarlos3@gmail.com>
2022-11-23 16:54:30 +0800

    comment added to route

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// route for teacher homepage
 Route::get('/teacher', function () {
     return view('homepage/index');
 });
