9779d6a3c5ddf6620305e9c45e5c8c1ce79600ac
Astra0023 <tambajhoncarlos3@gmail.com>
2022-11-24 14:42:29 +0800

    login button and register button added when user havent logged in yet

--- resources/views/layouts/teacher-navigation.blade.php
+++ resources/views/layouts/teacher-navigation.blade.php
@@ -20,69 +20,70 @@
           <li class="nav-item">
             <a class="nav-link text-dark" href="#">About Us</a>
           </li>
+          @if (Route::has('login'))
+                    @auth
+                    {{-- Notification dropdown --}}
+                    <li class="dropdown">
+                        <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="far fa-bell"></i>
+                          </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li class="head text-light bg-dark">
+                                <div class="row">
+                                <div class="col-lg-12 col-sm-12 col-12">
+                                <span>Notifications (3)</span>
+                                <a href="" class="float-right text-light">Mark all as read</a>
+                                </div>
+                                </li>
+                                <li class="notification-box">
+                                <div class="row">
+                                <div class="col-lg-3 col-sm-3 col-3 text-center">
+                                <img src="{{ URL('images/profile.png') }}" class="w-50 rounded-circle">
+                                </div>
+                                <div class="col-lg-8 col-sm-8 col-8">
+                                <strong class="text-info">John Doe</strong>
+                                <div>
+                                Lorem ipsum dolor sit amet, consectetur
+                                </div>
+                                <small class="text-warning">27.11.2015, 15:00</small>
+                                </div>
+                                </div>
+                                </li>
+                                <li class="notification-box bg-gray">
+                                <div class="row">
+                                <div class="col-lg-3 col-sm-3 col-3 text-center">
+                                <img src="{{ URL('images/profile.png') }}" class="w-50 rounded-circle">
+                                </div>
+                                <div class="col-lg-8 col-sm-8 col-8">
+                                <strong class="text-info">David John</strong>
+                                <div>
+                                Lorem ipsum dolor sit amet, consectetur
+                                </div>
+                                <small class="text-warning">27.11.2015, 15:00</small>
+                                </div>
+                                </div>
+                                </li>
+                                <li class="notification-box">
+                                <div class="row">
+                                <div class="col-lg-3 col-sm-3 col-3 text-center">
+                                <img src="{{ URL('images/profile.png') }}" class="w-50 rounded-circle">
+                                </div>
+                                <div class="col-lg-8 col-sm-8 col-8">
+                                <strong class="text-info">David John</strong>
+                                <div>
+                                Lorem ipsum dolor sit amet, consectetur
+                                </div>
+                                <small class="text-warning">27.11.2015, 15:00</small>
+                                </div>
+                                </div>
+                                </li>
+                                <li class="footer bg-dark text-center">
+                                <a href="" class="text-light">View All</a>
+                                </li>
+                                </ul>
+                      </li>
 
-          {{-- Notification dropdown --}}
-          <li class="dropdown">
-            <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="far fa-bell"></i>
-              </button>
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li class="head text-light bg-dark">
-                    <div class="row">
-                    <div class="col-lg-12 col-sm-12 col-12">
-                    <span>Notifications (3)</span>
-                    <a href="" class="float-right text-light">Mark all as read</a>
-                    </div>
-                    </li>
-                    <li class="notification-box">
-                    <div class="row">
-                    <div class="col-lg-3 col-sm-3 col-3 text-center">
-                    <img src="{{ URL('images/profile.png') }}" class="w-50 rounded-circle">
-                    </div>
-                    <div class="col-lg-8 col-sm-8 col-8">
-                    <strong class="text-info">John Doe</strong>
-                    <div>
-                    Lorem ipsum dolor sit amet, consectetur
-                    </div>
-                    <small class="text-warning">27.11.2015, 15:00</small>
-                    </div>
-                    </div>
-                    </li>
-                    <li class="notification-box bg-gray">
-                    <div class="row">
-                    <div class="col-lg-3 col-sm-3 col-3 text-center">
-                    <img src="{{ URL('images/profile.png') }}" class="w-50 rounded-circle">
-                    </div>
-                    <div class="col-lg-8 col-sm-8 col-8">
-                    <strong class="text-info">David John</strong>
-                    <div>
-                    Lorem ipsum dolor sit amet, consectetur
-                    </div>
-                    <small class="text-warning">27.11.2015, 15:00</small>
-                    </div>
-                    </div>
-                    </li>
-                    <li class="notification-box">
-                    <div class="row">
-                    <div class="col-lg-3 col-sm-3 col-3 text-center">
-                    <img src="{{ URL('images/profile.png') }}" class="w-50 rounded-circle">
-                    </div>
-                    <div class="col-lg-8 col-sm-8 col-8">
-                    <strong class="text-info">David John</strong>
-                    <div>
-                    Lorem ipsum dolor sit amet, consectetur
-                    </div>
-                    <small class="text-warning">27.11.2015, 15:00</small>
-                    </div>
-                    </div>
-                    </li>
-                    <li class="footer bg-dark text-center">
-                    <a href="" class="text-light">View All</a>
-                    </li>
-                    </ul>
-          </li>
-
-          {{-- Profile dropdown --}}
+                      {{-- Profile dropdown --}}
           <li class="dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                 <i class="far fa-user-circle"></i>
@@ -92,6 +93,14 @@
               <li><a class="dropdown-item" href="#">Tuition Status</a></li>
               <li><a class="dropdown-item" href="#">Logout</a></li>
             </ul>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+            @endif
 
         </li>
         </ul>
