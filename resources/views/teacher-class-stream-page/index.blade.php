ecff937e144d87d549c24c72d51dd097d9a820ec
Astra0023 <tambajhoncarlos3@gmail.com>
2022-11-26 01:50:13 +0800

    class list, class work, class stream done

--- resources/views/teacher-class-stream-page/index.blade.php
+++ resources/views/teacher-class-stream-page/index.blade.php
@@ -6,15 +6,15 @@
     {{-- Tab Header --}}
     <nav>
         <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-            <li><a class="nav-item nav-link class-stream active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Class Stream</a></li>
+            <li><a class="nav-item nav-link class-stream" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Class Stream</a></li>
             <li><a class="nav-item nav-link class-list" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Class List</a></li>
-            <li><a class="nav-item nav-link class-work" id="nav-contact-tab" data-bs-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Class Work</a></li>
+            <li><a class="nav-item nav-link class-work active" id="nav-contact-tab" data-bs-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Class Work</a></li>
         </div>
     </nav>
 
     {{-- Tab content --}}
     <div class="tab-content py-3 px-3 border border-1" id="a1">
-        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+        <div class="tab-pane fade" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                 <div class="row">
                     <div class="col">
                     <button class="btn btn-primary btn-lg" style="width: 200px;border: 1px;">
@@ -47,12 +47,51 @@
             </div>
         </div>
     </div>
-        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-            Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+        <div class="tab-pane fade show active" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+            <div class="row">
+                <div class="col">
+            <div class="card mb-3 p-4">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-row align-items-center">
+                      <div>
+                        <img
+                          src="{{ URL('images/profile.png') }}"
+                          class="img-fluid rounded-3" alt="Proifle" style="width: 65px;">
+                      </div>
+                      <div class="ms-3">
+                        <h5><strong>Activity Name</strong></h5>
+                        <p class="small mb-0">Students Name/Email</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
-        <div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
-            Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+        <div class="row">
+            <div class="col">
+                <div class="card mb-3 p-4">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-row align-items-center">
+                          <div>
+                            <img
+                              src="{{ URL('images/profile.png') }}"
+                              class="img-fluid rounded-3" alt="Proifle" style="width: 65px;">
+                          </div>
+                          <div class="ms-3">
+                            <h5><strong>Activity Name</strong></h5>
+                            <p class="small mb-0">Students Name/Email</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
         </div>
+        </div>
+       
     </div>
 </div>
 
