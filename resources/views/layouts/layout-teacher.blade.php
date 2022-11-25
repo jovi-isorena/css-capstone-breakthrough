f209f756b0a5f48b3ae8dec3e928a25784bd25ab
Astra0023 <tambajhoncarlos3@gmail.com>
2022-11-25 23:10:21 +0800

    footer in login page removed

--- resources/views/layouts/layout-teacher.blade.php
+++ resources/views/layouts/layout-teacher.blade.php
@@ -145,6 +145,8 @@ body{
 
             @yield('content')
 <br>
+@if (Route::has('login'))
+@auth
 <!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted">
 
@@ -229,6 +231,9 @@ body{
 
   </footer>
   <!-- Footer -->
+  @else
+  @endauth
+  @endif
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
