a352f79ae5dae28a4d20bbfb01d9818e510836b4
Astra0023 <tambajhoncarlos3@gmail.com>
2022-11-28 09:26:21 +0800

    teacher class-list-detail updated

--- resources/views/teacher-class-list-detail-page/index.blade.php
+++ resources/views/teacher-class-list-detail-page/index.blade.php
@@ -3,148 +3,41 @@
 
 {{-- Tabke Header --}}
 <div class="container-fluid mt-5">
-<div class="card">
-    <div class="card-header" style="background-color: #fff;">
-        <h6 class="text-center mb-0">Section 1 - Year 1</h6>
+    <div class="text-start mt-3 mb-5 ms-5">
+        <h2>Section 1 - Year 1</h2>
     </div>
-
+<div class="card border border-0">
 {{-- Table body --}}
     <div class="card-body">
   <div class="table-responsive">
-    <table class="table" style="width:100%">
+    <table class="table border border-3 rounded-0" style="width:100%">
            <thead>
       <tr>
-         <th>Studet Num</th>
-          <th>Name</th>
-          <th>Section</th>
-          <th>Subject 1</th>
-          <th>Subject 2</th>
-          <th>Subject 3</th>
-          <th>Subject 4</th>
-          <th>Subject 5</th>
-          <th>Subject 6</th>
-          <th>Subject 7</th>
-          <th>Subject 8</th>
-          <th>Final Grade</th>
-          <th>Release</th>
+         <th class="w-15 text-center border-end">Photo</th>
+          <th class="w-85 text-center">Student Name</th>
       </tr>
     </thead>
     <tbody>
         <tr>
-            <th scope="row">1</th>
-            <td>Student 1</td>
-            <td>Sample Section</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td></td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>
-            <button class="btn btn-primary mb-2" style="width: 100px;border-radius: 0;">
-                Yes
-            </button>
-            <br>
-            <button class="btn btn-danger" style="width: 100px;border-radius: 0;">
-                No
-            </button> 
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Student 2</td>
-            <td>Sample Section</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>
-                <button class="btn btn-primary mb-2" style="width: 100px;border-radius: 0;">
-                    Yes
-                </button>
-                <br>
-                <button class="btn btn-danger" style="width: 100px;border-radius: 0;">
-                    No
-                </button> 
-                </td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Student 3</td>
-            <td>Sample Section</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>
-                <button class="btn btn-primary mb-2" style="width: 100px;border-radius: 0;">
-                    Yes
-                </button>
-                <br>
-                <button class="btn btn-danger" style="width: 100px;border-radius: 0;">
-                    No
-                </button> 
-                </td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>Student 4</td>
-            <td>Sample Section</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>
-                <button class="btn btn-primary mb-2" style="width: 100px;border-radius: 0;">
-                    Yes
-                </button>
-                <br>
-                <button class="btn btn-danger" style="width: 100px;border-radius: 0;">
-                    No
-                </button> 
-                </td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>Student 5</td>
-            <td>Sample Section</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>80</td>
-            <td>
-                <button class="btn btn-primary mb-2" style="width: 100px;border-radius: 0;">
-                    Yes
-                </button>
-                <br>
-                <button class="btn btn-danger" style="width: 100px;border-radius: 0;">
-                    No
-                </button> 
-                </td>
-          </tr>
+            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
+            <th class="text-center align-middle text-secondary">Student 1</th>
+        </tr>
+        <tr>
+            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
+            <th class="text-center align-middle text-secondary">Student 2</th>
+        </tr>
+        <tr>
+            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
+            <th class="text-center align-middle text-secondary">Student 3</th>
+        </tr>
+        <tr>
+            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
+            <th class="text-center align-middle text-secondary">Student 4</th>
+        </tr>
+        <tr>
+            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
+            <th class="text-center align-middle text-secondary">Student 5</th>
+        </tr>
     </tbody>
     </table>
    </div>
