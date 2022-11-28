4eb82ad2453fafb1c326647c34ab4f345cb75cf6
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2022-11-28 18:46:49 +0800

    fix: added columns for login attempts, mandatory password change in user table. recreated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,82 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $email_verified_at
+ * @property string $password
+ * @property boolean $mandatoryChangePassword
+ * @property string $lastLoginAttempt
+ * @property integer $loginAttempCount
+ * @property string $lockedUntil
+ * @property string $remember_token
+ * @property string $role
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $status
+ * @property ConferenceSessionAttendance[] $conferenceSessionAttendances
+ * @property Notification[] $notifications
+ * @property Schooladmin[] $schooladmins
+ * @property Student[] $students
+ * @property Sysadmin[] $sysadmins
+ * @property Teacher[] $teachers
+ */
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'email_verified_at', 'password', 'mandatoryChangePassword', 'lastLoginAttempt', 'loginAttempCount', 'lockedUntil', 'remember_token', 'role', 'created_at', 'updated_at', 'status'];
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public function conferenceSessionAttendances()
+    {
+        return $this->hasMany('App\Models\ConferenceSessionAttendance', 'participantID');
+    }
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function notifications()
+    {
+        return $this->hasMany('App\Models\Notification', 'userID');
+    }
 
     /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function schooladmins()
+    {
+        return $this->hasMany('App\Models\Schooladmin', 'userID');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function students()
+    {
+        return $this->hasMany('App\Models\Student', 'userID');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sysadmins()
+    {
+        return $this->hasMany('App\Models\Sysadmin', 'userID');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function teachers()
+    {
+        return $this->hasMany('App\Models\Teacher', 'userID');
+    }
 }
