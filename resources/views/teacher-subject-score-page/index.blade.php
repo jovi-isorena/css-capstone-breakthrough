1892dd43191d8d4b91478f9f0242b951b1529e51
Astra0023 <tambajhoncarlos3@gmail.com>
2022-11-28 09:49:01 +0800

    teacher subject score page done

--- resources/views/teacher-subject-score-page/index.blade.php
+++ resources/views/teacher-subject-score-page/index.blade.php
@@ -3,42 +3,52 @@
 
 {{-- Tabke Header --}}
 <div class="container-fluid mt-5">
-    <div class="text-start mt-3 mb-5 ms-5">
-        <h2>Section 1 - Year 1</h2>
+    <div class="text-start mt-3 mb-5">
+        <h2>Teacher's Name</h2>
     </div>
 <div class="card border border-0">
 {{-- Table body --}}
     <div class="card-body">
   <div class="table-responsive">
-    <table class="table border border-3 rounded-0" style="width:100%">
+    <table class="table table-bordered border-3 rounded-0" style="width:100%">
            <thead>
       <tr>
-         <th>Student</th>
-          <th>Quizes</th>
-          <th>Assignment</th>
-          <th>Exams</th>
+        <th class="text-center">Student</th>
+        <th class="text-center">Quizes</th>
+        <th class="text-center">Assignment</th>
+        <th class="text-center">Exams</th>
       </tr>
     </thead>
     <tbody>
         <tr>
-            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
-            <th class="text-center align-middle text-secondary">Student 1</th>
+            <th class="rounded-0 m-0 w-25 text-center"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"><p>Student Name</p></th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
         </tr>
         <tr>
-            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
-            <th class="text-center align-middle text-secondary">Student 2</th>
+            <th class="rounded-0 m-0 w-25 text-center"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"><p>Student Name</p></th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
         </tr>
         <tr>
-            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
-            <th class="text-center align-middle text-secondary">Student 3</th>
+            <th class="rounded-0 m-0 w-25 text-center"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"><p>Student Name</p></th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
         </tr>
         <tr>
-            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
-            <th class="text-center align-middle text-secondary">Student 4</th>
+            <th class="rounded-0 m-0 w-25 text-center"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"><p>Student Name</p></th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
         </tr>
         <tr>
-            <th class="border-end rounded-0 m-0 w-25"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"></th>
-            <th class="text-center align-middle text-secondary">Student 5</th>
+            <th class="rounded-0 m-0 w-25 text-center"><img src="{{ URL('images/profile.png') }}" class="img-fluid rounded-3" alt="Proifle" style="width: 100px;"><p>Student Name</p></th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
+            <th class="text-center align-middle text-secondary">80</th>
         </tr>
     </tbody>
     </table>
