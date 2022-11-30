ab097d7b895a13eb54bdc4a32f9e88b45e5ade4f
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2022-11-30 10:11:53 +0800

    feat: created 4 navbar for each user levels

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,11 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            {{-- @include('layouts.navigation') --}}
+            {{-- @include('layouts.sysad-navbar') --}}
+            {{-- @include('layouts.teacher-navbar') --}}
+            {{-- @include('layouts.student-navbar') --}}
+            @include('layouts.schladmin-navbar')
 
             <!-- Page Heading -->
             @if (isset($header))
