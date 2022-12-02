e0d6390ba3828672d41fb812e6f2acbd8a390d27
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2022-12-02 16:48:39 +0800

    style: moved header to navbar to make it sticky

--- resources/views/layouts/student-navbar.blade.php
+++ resources/views/layouts/student-navbar.blade.php
@@ -106,4 +106,12 @@
             </div>
         </div>
     </div>
+    <!-- Page Heading -->
+    @if (isset($header))
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+    @endif
 </nav>
