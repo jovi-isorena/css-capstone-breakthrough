287a89b503097cf4204b8357054322078651efef
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2022-12-02 09:15:28 +0800

    style: added breakthrough icon in webpage title, navbar and welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- icon -->
+        <link rel="icon" href="/BreakThrough.ico" type="image/icon type">
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
         <!-- Scripts -->
         <script src="https://kit.fontawesome.com/9a8a7844ac.js" crossorigin="anonymous"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
