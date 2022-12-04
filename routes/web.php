3d2aa46d7edfb893bfdb3635f474d2a6b5d20a13
Astra0023 <tambajhoncarlos3@gmail.com>
2022-12-04 15:27:34 +0800

    route: Add route for class-stream class list

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::middleware('auth')->group(function () {
     //accounts
     Route::get('sysadmin/accounts', [UserController::class, 'index'])->name('accounts');
     Route::post('sysadmin/account', [UserController::class, 'store'])->name('accountStore');
-    
+
     //curriculum
     Route::get('sysadmin/curriculums', [CurriculumController::class, 'index'])->name('curriculums');
     Route::get('sysadmin/curriculum/create', [CurriculumController::class, 'create'])->name('curriculumCreate');
@@ -51,7 +51,7 @@ Route::middleware('auth')->group(function () {
     Route::get('sysadmin/curriculum/{curriculum}', [CurriculumController::class, 'show'])->name('curriculumShow');
     Route::get('sysadmin/curriculum/{curriculum}/edit', [CurriculumController::class, 'edit'])->name('curriculumEdit');
     Route::put('sysadmin/curriculum/{curriculum}/update', [CurriculumController::class, 'update'])->name('curriculumUpdate');
-    
+
     //schoolyear
     Route::get('sysadmin/schoolYears', [SchoolYearController::class, 'index'])->name('schoolYears');
     Route::get('sysadmin/schoolYear/create', [SchoolYearController::class, 'create'])->name('schoolYearCreate');
@@ -59,7 +59,7 @@ Route::middleware('auth')->group(function () {
     Route::get('sysadmin/schoolYear/{schoolYear}', [SchoolYearController::class, 'show'])->name('schoolYearShow');
     Route::get('sysadmin/schoolYear/{schoolYear}/edit', [SchoolYearController::class, 'edit'])->name('schoolYearEdit');
     Route::put('sysadmin/schoolYear/{schoolYear}/update', [SchoolYearController::class, 'update'])->name('schoolYearUpdate');
-    
+
     //sections
     Route::get('sysadmin/sections', [SectionController::class, 'index'])->name('sections');
     Route::get('sysadmin/section/create', [SectionController::class, 'create'])->name('sectionCreate');
@@ -69,10 +69,10 @@ Route::middleware('auth')->group(function () {
     Route::put('sysadmin/section/{section}/update', [SectionController::class, 'update'])->name('sectionUpdate');
     Route::put('sysadmin/section/{section}/updateName', [SectionController::class, 'updateName'])->name('sectionUpdateName');
     Route::put('sysadmin/section/{section}/updateAdviser', [SectionController::class, 'updateAdviser'])->name('sectionUpdateAdviser');
-    
+
     // section students
     Route::post('sysadmin/sectionStudent/store', [SectionStudentController::class, 'store'])->name('sectionStudentStore');
-    
+
     // section subjects
     Route::put('sysadmin/sectionSubject/teachers/update', [SectionSubjectController::class, 'updateTeachers'])->name('sectionUpdateTeachers');
 
@@ -81,43 +81,46 @@ Route::middleware('auth')->group(function () {
     // Route::get('/teacher', function () {
     //     return view('teacher/home-page/index');
     // });
-    
+
     // route for teacher login
     // Route::get('/teacher/login', function () {
     //     return view('teacher/login-page/index');
     // });
-    
-    
+
+
     // route for teacher classes
     Route::get('/teacher/classes', [TeacherController::class, 'classes'])->name('teacherClasses');
-    
+
     // route for teacher class stream page
     Route::get('/teacher/class-stream', [TeacherController::class, 'classStream'])->name('teacherClassStream');
-    
+
+    //Show selected section
+    Route::get('/teacher/class-stream/{section}', [TeacherController::class, 'classshow'])->name('teacherstudentshow');
+
     // route for teacher class activiy page
     Route::get('/teacher/class-activity', [TeacherController::class, 'classActivity'])->name('teacherClassActivity');
-    
+
     // route for teacher class list detail page
     Route::get('/teacher/class-list-detail', [TeacherController::class, 'classListDetail'])->name('teacherClassListDetail');
-    
+
     // route for teacher class subject score page
     Route::get('/teacher/subject-score', [TeacherController::class, 'subjectScore'])->name('teacherSubjectScore');
-    
+
     // route for teacher posted task page
     Route::get('/teacher/posted-task', [TeacherController::class, 'postedTask'])->name('teacherPostedTask');
-    
+
     // route for teacher posted task subject page
     Route::get('/teacher/posted-task-subject', [TeacherController::class, 'postedTaskSubject'])->name('teacherPostedTaskSubject');
-    
+
     // route for teacher posted task section list page
     Route::get('/teacher/posted-task-section', [TeacherController::class, 'postedTaskSection'])->name('teacherPostedTaskSection');
-    
+
     // route for teacher posted task subject student
     Route::get('/teacher/posted-task-subject-student', [TeacherController::class, 'postedTaskSubjectStudent'])->name('teacherPostedTaskSubjectStudent');
-    
+
     // route for teacher create task page
     Route::get('/teacher/create-task', [TeacherController::class, 'createTask'])->name('teacherCreateTask');
-    
+
     // route for teacher create task page
     // Route::get('/teacher/profile', function () {
     //     return view('teacher/profile-page/index');
