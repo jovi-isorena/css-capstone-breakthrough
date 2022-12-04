96a091f77cf7508e531b86a2feac789e05759f50
Astra0023 <tambajhoncarlos3@gmail.com>
2022-12-04 15:33:00 +0800

    Feat: display section list for teacher

--- resources/views/teacher/classes-page/index.blade.php
+++ resources/views/teacher/classes-page/index.blade.php
@@ -8,54 +8,47 @@
     </x-slot>
     <section class="h-100">
         <div class="container h-100 py-5">
+            <h1>
+
+            </h1>
+
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-10">
 
+                @for ($i = 1; $i < $teacherSectionCounts; $i++)
+                @if ($SectionSubjects->where('sectionID', $i)->count() > 0)
             <div class="card rounded-3 mb-4">
                 <div class="card-body p-4">
                 <div class="row d-flex justify-content-between align-items-center">
                     <div class="col-md-3 col-lg-3 col-xl-3">
-                    <p class="lead fw-normal mb-2">Subject 1</p>
-                    <p style="margin: 0;">Section</p>
-                    <p style="margin: 0;">Information</p>
+                        @foreach ($SectionSubjects->where('sectionID', $i) as $SectionSubject)
+                    <p class="lead fw-normal mb-2">
+                    </p>
+                {{ $SectionSubject->name }}
+                    <p style="margin: 0;">
+                {{ $SectionSubject->Section->name }}
+
+                    </p>
+
+                    <p style="margin: 0;">
+                        {{ $SectionSubject->gradeLevel }}
+                        <span class="ms-3">
+                            {{ $SectionSubject->sectionStudent->distinct()->count('sectionID'); }}
+
+                        </span>
+
+                    </p>
+                    @endforeach
                     </div>
                     <div class="col-md-2 col-lg-2 col-xl-2 text-end">
-                        <img src="{{ URL('images/placeholder.jpg') }}" class="img-fluid rounded-3" alt="Place Holder">
+                       <a href="{{ route('teacherstudentshow', $SectionSubject->sectionID) }}" class="stretched-link" ><img src="{{ URL('images/placeholder.jpg') }}" class="img-fluid rounded-3" alt="Place Holder"></a>
                     </div>
                 </div>
                 </div>
-            </div>
 
-            <div class="card rounded-3 mb-4">
-                <div class="card-body p-4">
-                <div class="row d-flex justify-content-between align-items-center">
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                    <p class="lead fw-normal mb-2">Subject 2</p>
-                    <p style="margin: 0;">Section</p>
-                    <p style="margin: 0;">Information</p>
-                    </div>
-                    <div class="col-md-2 col-lg-2 col-xl-2 text-end">
-                        <img src="{{ URL('images/placeholder.jpg') }}" class="img-fluid rounded-3" alt="Place Holder">
-                    </div>
-                </div>
-                </div>
             </div>
-
-            <div class="card rounded-3 mb-4">
-                <div class="card-body p-4">
-                <div class="row d-flex justify-content-between align-items-center">
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                    <p class="lead fw-normal mb-2">Subject 3</p>
-                    <p style="margin: 0;">Section</p>
-                    <p style="margin: 0;">Information</p>
-                    </div>
-                    <div class="col-md-2 col-lg-2 col-xl-2 text-end">
-                        <img src="{{ URL('images/placeholder.jpg') }}" class="img-fluid rounded-3" alt="Place Holder">
-                    </div>
-                </div>
-                </div>
-            </div>
-
+@endif
+@endfor
 
             </div>
         </div>
