68f0ab1597fbfb8c4e8cf3f3a21538d69a2509d0
Astra0023 <tambajhoncarlos3@gmail.com>
2023-01-02 15:41:38 +0800

    Added: file for modal added : archieve accounts

--- resources/views/sysadmin/accounts/index.blade.php
+++ resources/views/sysadmin/accounts/index.blade.php
@@ -22,7 +22,7 @@
         @include('sysadmin.accounts.partials.createSysadmin')
         @include('sysadmin.accounts.partials.createTeacher')
         @include('sysadmin.accounts.partials.createStudent')
-               
+
         <div class="my-5">
             <table class="table text-center">
                 <thead>
@@ -36,17 +36,19 @@
                 <tbody>
                     @foreach ($users as $user)
                         <tr>
-                            <td>{{$user->id}}</td>    
-                            <td>{{$user->name}}</td>    
-                            <td>{{$user->role}}</td>    
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->role}}</td>
                             <td>
                                 <a href="{{ route('accountShow', $user->id) }}" class="btn btn-primary">View</a>
-                                <button class="btn btn-danger">Archive</button>
+                                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#archieveModal">Archive</button>
                             </td>
-                        </tr>    
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
     </div>
+    @include('sysadmin.accounts.partials.archieveaccounts')
+
 </x-app-layout>
---
+++ resources/views/sysadmin/accounts/partials/archieveaccounts.blade.php
@@ -0,0 +1,18 @@
+ <!-- Modal -->
+ <div class="modal fade" id="archieveModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Archieve</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+         <h1>Do you want to archieve?</h1>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Archieve</button>
+        </div>
+      </div>
+    </div>
+  </div>
